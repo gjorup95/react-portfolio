92cbecb3a9ba70d7e8af4ad51d77f7754754d577
Troels Gjorup <gjollerup@gmail.com>
2025-02-26 21:37:40 +0100

    Fixes portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,23 +1,32 @@
 import './index.scss'
-const Portfolio = ({ imageSrc, contributions }) => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+const Portfolio = ({ imageSrc, contributions, projectTitle, year, technologies }) => {
   return (
     <div className="grid-container-portfolio">
-      <h1 className="title-unique">Producentansvarsregisteret</h1>
-
-      <div className="image-container">
-        <img src={imageSrc} alt="Project" />
-      </div>
-
       <div className="text-container">
+        <div className="title-container">
+          <h1 className="title-unique">{projectTitle}</h1>
+          <h2 className="subtitle-year">{year}</h2>
+        </div>
+        <div className="image-container">
+          <img src={imageSrc} alt="Project" />
+        </div>
+
+
         <h2 className="override-h2">Key Contributions</h2>
         <ul className="override-ul">
           {contributions.map((item, index) => (
             <li className="override-li" key={index}>{item}</li>
           ))}
         </ul>
-      </div>
-      <div className="text-container">
+
         <h2 className="override-h2">Technologies</h2>
+        <div className="technology-container">
+          {technologies?.map((item, index) => (
+            <FontAwesomeIcon icon={item} size={'8x'} style={{ gridColumn: `${(index % 3) + 1} / span 1` }}
+                             color={'#ffd700'} alt={`technologies ${index}`} key={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Scroll/index.js
+++ src/components/Scroll/index.js
@@ -5,6 +5,9 @@ import ScrollableContainer from '../ScrollableContainer'
 import React from 'react'
 import PortfolioLoadingScreen from '../PortfolioLoadingScreen'
 import Portfolio from '../Portfolio'
+import { faBootstrap, faJava, faJs } from '@fortawesome/free-brands-svg-icons'
+import { faAngular } from '@fortawesome/free-brands-svg-icons/faAngular'
+import { faMicrosoft } from '@fortawesome/free-brands-svg-icons/faMicrosoft'
 
 
 class ScrollPage extends React.Component {
@@ -20,7 +23,24 @@ class ScrollPage extends React.Component {
                        "Implemented responsive UI",
                        "Optimized backend performance",
                        "Integrated third-party APIs",
-                     ]}></Portfolio>
+                     ]} projectTitle={"Producentansvarsregisteret"} year={"2024 – Present"} technologies={[faAngular, faMicrosoft, faJs ]}>
+          </Portfolio>
+
+          <Portfolio imageSrc="https://placehold.co/600x400"
+                     contributions={[
+                       "Implemented responsive UI",
+                       "Optimized backend performance",
+                       "Integrated third-party APIs",
+                     ]} projectTitle={"Digital tinglysning"} year={"2023 – 2024"} technologies={[faJava, faAngular, faBootstrap]}>>
+          </Portfolio>
+
+          <Portfolio imageSrc="https://placehold.co/600x400"
+                     contributions={[
+                       "Implemented responsive UI",
+                       "Optimized backend performance",
+                       "Integrated third-party APIs",
+                     ]} projectTitle={"MitDK"} year={"2023 – 2023"} technologies={[faJava, faAngular, faBootstrap]}>>
+          </Portfolio>
 
         </ScrollableContainer>
       </>
