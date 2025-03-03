661134959f20e061ec166b56269ece0e9760c2c5
Troels Gjorup <gjollerup@gmail.com>
2025-03-03 16:17:12 +0100

    Updates portfolio

--- src/components/Scroll/index.js
+++ src/components/Scroll/index.js
@@ -5,13 +5,15 @@ import ScrollableContainer from '../ScrollableContainer'
 import React from 'react'
 import PortfolioLoadingScreen from '../PortfolioLoadingScreen'
 import Portfolio from '../Portfolio'
-import { faBootstrap, faJava, faJs, faReact } from '@fortawesome/free-brands-svg-icons'
+import {faJava, faReact } from '@fortawesome/free-brands-svg-icons'
 import { faAngular } from '@fortawesome/free-brands-svg-icons/faAngular'
 import mitDKImage from '../../assets/images/MitDK.png'
 import logoKeycloak from '../../assets/images/svg/Logo_of_Keycloak.svg'
 import mySqlLogo from '../../assets/images/svg/mysqlLogo.svg'
 import { logoAspNet } from '../../utils/FontAwesomeIcons'
 import sentryLogo from '../../assets/images/svg/springLogo.svg'
+import digitalTinglysningImage from '../../assets/images/DigitalTinglysningNew.png'
+import par2Image from '../../assets/images/Par2.png'
 
 class ScrollPage extends React.Component {
   render() {
@@ -21,27 +23,27 @@ class ScrollPage extends React.Component {
           <section id="part1">
             <PortfolioLoadingScreen />
           </section>
-          <Portfolio imageSrc="https://placehold.co/600x400"
+          <Portfolio imageSrc={par2Image}
                      contributions={[
-                       "Implemented responsive UI",
-                       "Optimized backend performance",
-                       "Integrated third-party APIs",
+                       "Setup Nemlogin with keycloak and allocated privileges for role based access control",
+                       "Integrated with 3rd party vendors and setup endpoints for authorized external data access",
+                       "Developed a new feature for creating, storing and accessing business agreements between eligible parties",
                      ]} projectTitle={"Producentansvarsregisteret"} year={"2024 – Present"} technologies={[faAngular, logoAspNet, logoKeycloak ]}>
           </Portfolio>
 
-          <Portfolio imageSrc="https://placehold.co/600x400"
+          <Portfolio imageSrc={digitalTinglysningImage}
                      contributions={[
-                       "Implemented responsive UI",
-                       "Optimized backend performance",
-                       "Integrated third-party APIs",
+                       "Developed new, responsive, WCAG compatible document types for user completion of legal documents",
+                       "Created 3rd party integration, with event-based publishing of internal documents for public access",
+                       "Developed a new case handling flow with pagination, searching and filtering with angular and ngrx store",
                      ]} projectTitle={"Digital tinglysning"} year={"2023 – 2024"} technologies={[faJava, faAngular, mySqlLogo]}>>
           </Portfolio>
 
           <Portfolio imageSrc={mitDKImage}
           contributions={[
-                       "Implemented responsive UI",
-                       "Optimized backend performance",
-                       "Integrated third-party APIs",
+                       "Synchronised public and private user types for a unified mail experience",
+                       "Developed new customizable forms for company user research",
+                       "Sentry/Kibana log monitoring and debugging across microservices and react native app",
                      ]} projectTitle={"MitDK"} year={"2023 – 2023"} technologies={[faJava, faReact, sentryLogo]}>>
           </Portfolio>
 
